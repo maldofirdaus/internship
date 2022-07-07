6a68ec9466ebecd2dc7165af336afff1f1816c53
nyayu27 <nurqomariahnyayu@gmail.com>
2022-07-07 11:21:30 +0700

    internship

--- apiget.php
+++ apiget.php
@@ -1,13 +1,14 @@
- <?php
+<?php
 
 header('Content-Type: application/json');
 
 date_default_timezone_set("Asia/Jakarta");
 $nama = htmlspecialchars($_GET['nama']);
+$nama_belakang = htmlspecialchars($_GET['nama_belakang']);
 $waktu = date('Y/m/d H:i');
 
 $data = [
-    'nama' => $nama,
+    'nama' => "$nama $nama_belakang",
     'waktu' => $waktu
 ];
 
